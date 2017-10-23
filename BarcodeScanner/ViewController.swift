7111f0dd730948b3525d4152a497162423c85f48
Christian Dobrovolny <funkyman@ok.de>
2017-10-23 21:04:16 +0200

    add barcode capture functionality

--- BarcodeScanner/ViewController.swift
+++ BarcodeScanner/ViewController.swift
@@ -7,19 +7,89 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var flashButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    // first steps:
+    var captureDevice : AVCaptureDevice?
+    var captureSession : AVCaptureSession?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    var greenbox : UIView?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = captureMetadataOutput.availableMetadataObjectTypes
+            
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            previewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(previewLayer!)
+            
+            
+            greenbox = UIView()
+            
+            if let qrCodeFrameView = greenbox {
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrCodeFrameView)
+                view.bringSubview(toFront: qrCodeFrameView)
+            }
+
+            captureSession?.startRunning()
+            
+            view.bringSubview(toFront: flashButton)
+        } catch {
+            fatalError()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
-
+    
+    @IBAction func activateFlash(_ sender: UIButton) {
+        try! captureDevice?.lockForConfiguration()
+        if (captureDevice?.hasTorch)! {
+            if(captureDevice?.isTorchActive)! {
+                captureDevice?.torchMode =  AVCaptureDevice.TorchMode.off
+            } else {
+                captureDevice?.torchMode = AVCaptureDevice.TorchMode.on
+            }
+        }
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        if metadataObjects.count == 0 {
+            greenbox?.frame = CGRect.zero
+        } else {
+            if let la = metadataObjects.first as? AVMetadataMachineReadableCodeObject {
+                let barcodeObject = previewLayer?.transformedMetadataObject(for: la)
+                greenbox?.frame = (barcodeObject?.bounds)!
+                view.bringSubview(toFront: label)
+                label.text = la.stringValue
+                print("captured")
+            }
+        }
+    }
 }
 
